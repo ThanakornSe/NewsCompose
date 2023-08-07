2ff7b73a3ebd1baef645a3cf250dc3af1b44a6ad
ThanakornSe <huang.thanakorn@gmail.com>
2023-08-07 10:12:18 +0700

    -fix app crash

--- core/util/src/main/java/com/exam/application/core/util/StringUtils.kt
+++ core/util/src/main/java/com/exam/application/core/util/StringUtils.kt
@@ -4,7 +4,7 @@ import kotlin.io.encoding.Base64
 import kotlin.io.encoding.ExperimentalEncodingApi
 
 @OptIn(ExperimentalEncodingApi::class)
-fun String.encodeBase64(): String = Base64.encode(this.toByteArray())
+fun String.encodeBase64(): String = Base64.UrlSafe.encode(this.toByteArray())
 
 @OptIn(ExperimentalEncodingApi::class)
-fun String.decodeBase64(): String = String(Base64.decode(this))
+fun String.decodeBase64(): String = String(Base64.UrlSafe.decode(this))
